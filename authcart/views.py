8f369e3cf9de02f8bd227a0dff4fdbe926d501c0
Cole Tran <mathlover1998@gmail.com>
2023-04-11 09:33:13 +0700

    Add signUp function, add forms.py

--- authcart/views.py
+++ authcart/views.py
@@ -1,32 +1,39 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
-# Create your views here.
+from .forms import RegisterForm
+from django.contrib.auth import login, logout
+
+
 
 def signup(request):
-    if request.method =='POST':
-        email = request.POST['email']
-        if User.objects.filter(username = email):
-            messages.error(request, 'This email is taken!')
-            return redirect('/auth/signup')
-        else:
-            password = request.POST['password']
-            confirm_password = request.POST['confirm_password']
+    form = RegisterForm()
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            if User.objects.filter(username=email):
+                messages.error(
+                    request, 'This email is taken! Please log in instead!')
+                return redirect('handleLogin')
+            password = form.cleaned_data.get('password')
+            confirm_password = form.cleaned_data.get('confirm_password')
             if password != confirm_password:
-                messages.error(request,'This password confirmation does not match!')
-                return redirect('/auth/signup')
-            else:
-                user = User.objects.create_user(username=email,email=email,password=password)
-                user.save()
-                return redirect('index')
-                
+                messages.warning(
+                    request, 'Confirm password must be the same with password!')
+                return redirect('signUp')
+            user = User.objects.create_user(
+                username=email, email=email, password=password)
+            user.save()
+            login(request, user)
+            return redirect('index')
 
+    return render(request, 'authentication/login-signup.html',{"form":form,"is_login":False})
 
-    else:
-        return render(request,'authentication/signup.html')
 
 def handle_login(request):
-    return render(request,'authentication/login.html')
+    return render(request, 'authentication/login-signup.html')
+
 
 def handle_logout(request):
     return redirect('/auth/login')
